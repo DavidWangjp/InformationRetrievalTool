ac399aa5ed5964d5dffb7b5e95d3c13d480dcf30
seeForYou <641119121@qq.com>
2018-06-26 00:25:18 +0800

    mutil phrase query

--- src/QueryEntry.java
+++ src/QueryEntry.java
@@ -13,7 +13,7 @@ public class QueryEntry {
     //    (Brutus OR Caesar) AND NOT (Antony OR Cleopatra)
 
 
-    public static ArrayList andQuery(ArrayList<Integer> lpos, ArrayList<Integer> rpos){
+    public static ArrayList andQuery(List<Integer> lpos, List<Integer> rpos){
         ArrayList<Integer> res = new ArrayList();
         for(int i = 0; i < lpos.size(); ++ i){
             for(int j = 0; j < rpos.size(); ++j){
@@ -80,6 +80,33 @@ public class QueryEntry {
         return  res;
     }
 
+    public static List multiPhraseQuery(String...strings){
+        int length = strings.length;
+        List<Integer> result = new ArrayList();
+        LinkedHashMap<Integer, ArrayList<Integer>> res = new LinkedHashMap<>();
+        List<LinkedHashMap> retrieveRes = new ArrayList<>();
+        for(int i = 0; i < strings.length; ++i){
+            LinkedHashMap<Integer, ArrayList<Integer>> lres = RetrievalUtil.retrieveWord(strings[i]);
+            if(lres == null){
+                return null;
+            }
+            retrieveRes.add(lres);
+        }
+        for(int i = 0; i < length - 1; ++i){
+            res = QueryEntry.phraseQuery(retrieveRes.get(i), retrieveRes.get(i + 1));
+            List<Integer> temp = new ArrayList<>();
+            for(Map.Entry entry: res.entrySet()){
+                temp.add((Integer)entry.getKey());
+            }
+            if(i == 0){
+                result = temp;
+            }else{
+                result = andQuery(result, temp);
+            }
+        }
+        return result;
+    }
+
     public static ArrayList<Integer> getDocIds(LinkedHashMap<Integer, ArrayList<Integer>> t){
         ArrayList<Integer> res = new ArrayList();
         for (Map.Entry entry: t.entrySet()) {
@@ -97,32 +124,40 @@ public class QueryEntry {
 //            String word = scanner.next();
 //        }
         //grain board
-        String lword = "grain";
-        String rword = "board";
-        QueryEntry entry = new QueryEntry();
-        LinkedHashMap<Integer, ArrayList<Integer>> lres = RetrievalUtil.retrieveWord(lword);
-        LinkedHashMap<Integer, ArrayList<Integer>> rres = RetrievalUtil.retrieveWord(rword);
-        LinkedHashMap<Integer, ArrayList<Integer>> res = QueryEntry.phraseQuery(lres, rres);
+          //detailed export registrations
+          List<Integer> test = QueryEntry.multiPhraseQuery("detailed", "export", "registrations");
+          System.out.println("Mutil query test :");
+          for(int i = 0; i < test.size(); ++i) {
+              System.out.println("doc id = " + test.get(i));
+          }
 
-        ArrayList<Integer> lpos = QueryEntry.getDocIds(lres);
-        ArrayList<Integer> rpos = QueryEntry.getDocIds(rres);
 
-        ArrayList<Integer> andRes = QueryEntry.andQuery(lpos, rpos);
-        ArrayList<Integer> orRes = QueryEntry.orQuery(lpos, rpos);
-        ArrayList<Integer> notRes = QueryEntry.notQuery(lpos, rpos);
-
-        System.out.println("and query result:");
-        for(int i = 0; i < andRes.size(); ++i){
-            System.out.println("pos = " + andRes.get(i));
-        }
-        System.out.println("or query result:");
-        for(int i = 0; i < orRes.size(); ++i){
-            System.out.println("pos = " + orRes.get(i));
-        }
-        System.out.println("not query result:");
-        for(int i = 0; i < notRes.size(); ++i){
-            System.out.println("pos = " + notRes.get(i));
-        }
+//        String lword = "grain";
+//        String rword = "board";
+//        QueryEntry entry = new QueryEntry();
+//        LinkedHashMap<Integer, ArrayList<Integer>> lres = RetrievalUtil.retrieveWord(lword);
+//        LinkedHashMap<Integer, ArrayList<Integer>> rres = RetrievalUtil.retrieveWord(rword);
+//        LinkedHashMap<Integer, ArrayList<Integer>> res = QueryEntry.phraseQuery(lres, rres);
+//
+//        ArrayList<Integer> lpos = QueryEntry.getDocIds(lres);
+//        ArrayList<Integer> rpos = QueryEntry.getDocIds(rres);
+//
+//        ArrayList<Integer> andRes = QueryEntry.andQuery(lpos, rpos);
+//        ArrayList<Integer> orRes = QueryEntry.orQuery(lpos, rpos);
+//        ArrayList<Integer> notRes = QueryEntry.notQuery(lpos, rpos);
+//
+//        System.out.println("and query result:");
+//        for(int i = 0; i < andRes.size(); ++i){
+//            System.out.println("pos = " + andRes.get(i));
+//        }
+//        System.out.println("or query result:");
+//        for(int i = 0; i < orRes.size(); ++i){
+//            System.out.println("pos = " + orRes.get(i));
+//        }
+//        System.out.println("not query result:");
+//        for(int i = 0; i < notRes.size(); ++i){
+//            System.out.println("pos = " + notRes.get(i));
+//        }
 //        System.out.println("Phrase query for " + lword + " " + rword);
 //        for (Map.Entry entry2 : res.entrySet()) {
 //            Integer docId = (Integer) entry2.getKey();
